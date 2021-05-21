5b2138d9baceed5bce811b78b48209ca77fdb6be
Syrine Bahri <78451998+SyrineB11@users.noreply.github.com>
2021-05-21 17:25:37 +0200

    Update bac.cpp

--- bac.cpp
+++ bac.cpp
@@ -147,7 +147,7 @@ int Game::verif(string mot ,string nom_f,char c)
       else
       {
         while (entree >> n)
-        if (n==mot)
+        if ((n==mot) &&(mot.at(0)==c))
         {
           k=1;
         }
